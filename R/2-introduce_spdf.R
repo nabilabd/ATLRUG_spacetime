607847388e7be704f508474ac57f72d64c0b6750
Nabil A <nabilabdu@gatech.edu>
2016-02-20 09:15:55 -0500

    renamed file, made changes

---
+++ R/2-introduce_spdf.R
@@ -0,0 +1,39 @@
+
+source("R/1-data.R")
+
+library(magrittr)
+
+# to form an SPDF, identify the columns containing coordinates
+coordinates(meuse) <- ~ x + y
+meuse %>% class
+meuse %>% str
+
+
+# now has five slots. These can be accessed via helper functions: 
+bbox(meuse)
+identical(bbox(meuse), meuse@bbox)
+
+coordinates(meuse) %>% head(10)
+identical(coordinates(meuse), meuse@coords)
+
+proj4string(meuse)
+meuse@proj4string
+
+# data argument
+meuse@data %>% tbl_df
+meuse %>% as.data.frame %>% tbl_df
+
+
+# potential problems
+library(ggplot2)
+
+# there are two problems here...
+meuse %>% 
+  ggplot(aes(x, y, color = elev)) + geom_point() + coord_equal()
+
+# but this works
+meuse %>% 
+  as.data.frame %>% 
+  ggplot(aes(x, y, color = elev)) + geom_point() + coord_equal()
+
+
