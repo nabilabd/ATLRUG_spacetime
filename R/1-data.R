5340a9ae7b24f721ec8743f1f0671f05304e5ec9
Nabil A <nabilabdu@gatech.edu>
2016-02-20 09:38:12 -0500

    added graphing commands

--- R/1-data.R
+++ R/1-data.R
@@ -12,3 +12,8 @@ tbl_df(meuse.grid)
 
 meuse.grid %>% tbl_df %>% arrange(x, y)
 
+library(ggplot2)
+
+ggplot(meuse, aes(x, y)) + geom_point(size=1) + coord_equal()
+ggplot(meuse.grid, aes(x, y)) + geom_point(size=1) + coord_equal()
+
